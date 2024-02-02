29655effeb6982a028f11ff7a677c2b5d174c612
ush9211 <pg.ush9211@gmail.com>
2024-02-02 14:41:30 +0900

    list jquery works

--- js/script.js
+++ js/script.js
@@ -19,6 +19,17 @@ $(function(){
         // preventDefault : 기능 중지
         e.preventDefault();
         const view = $(this).data("view");
-        alert(view);
+        $('#pdlist>div').removeClass();
+        $('.listview rect').removeClass('list-act-color').addClass('list-color');
+        if(view==3){
+            $(this).find('rect').removeClass('list-color').addClass('list-act-color');
+            $('#pdlist>div').addClass("col-md-4 mb-5");
+        }else if(view==4){
+            $(this).find('rect').removeClass('list-color').addClass('list-act-color');
+            $('#pdlist>div').addClass("col-md-3 mb-5");
+        }else if(view==5){
+            $(this).find('rect').removeClass('list-color').addClass('list-act-color');
+            $('#pdlist>div').addClass("col-md-55 mb-5");
+        }
     });
 });
